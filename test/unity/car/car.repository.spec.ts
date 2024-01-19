9b5ab1003a3382653530891ea4428ff570f5a359
Javier Villca <javieranibalvillca@gmail.com>
2024-01-19 00:06:01 -0300

    test unitario de la capa repository del modulo car terminado

--- test/unity/car/car.repository.spec.ts
+++ test/unity/car/car.repository.spec.ts
@@ -4,6 +4,8 @@ import { CarRepository } from '../../../src/module/car/repository/car.repository
 import { DbCarDto } from 'src/module/car/dto/db.car.dto';
 import { DeleteResult } from 'typeorm';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { NewCarDto } from 'src/module/car/dto/new.car.dto';
+import { UpdateCarDto } from 'src/module/car/dto/update.car.dto';
 
 describe('CarRepository', () => {
   let carRepository: CarRepository;
@@ -22,6 +24,20 @@ describe('CarRepository', () => {
     createdAt: new Date(),
     updatedAt: new Date(),
   };
+  const mockNewCarDto: NewCarDto = {
+    brand: 'brand',
+    model: 'model',
+    year: 1995,
+    kms: 2000,
+    color: 'red',
+    passengers: 4,
+    price: 2000,
+    img: null,
+  };
+  const mockUpdateCarDto: UpdateCarDto = {
+    id: 5,
+    ...mockNewCarDto,
+  };
   const mockDeleteResult: DeleteResult = {
     raw: undefined,
     affected: 1,
@@ -30,6 +46,8 @@ describe('CarRepository', () => {
   const mockCarEntity = {
     find: jest.fn().mockReturnValue(mockCarsDto),
     findOne: jest.fn().mockReturnValue(mockCarDto),
+    create: jest.fn().mockReturnValue(mockNewCarDto),
+    preload: jest.fn().mockReturnValue(mockUpdateCarDto),
     save: jest.fn().mockReturnValue(mockCarDto),
     delete: jest.fn().mockReturnValue(mockDeleteResult),
   };
@@ -50,12 +68,60 @@ describe('CarRepository', () => {
   });
 
   describe('getCars', () => {
-    it('Calls TypeOrm find method and returns an array of DbCarDto entities', async () => {
+    it('Call TypeOrm find method and returns an array of DbCarDto entities', async () => {
       jest.spyOn(mockCarEntity, 'find');
       const result = await carRepository.getCars();
 
       expect(mockCarEntity.find).toHaveBeenCalled();
       expect(result).toEqual(mockCarsDto);
+    });
+  });
+
+  describe('getCar', () => {
+    it('Call the TypeOrm findOne method and returns a DbCarDto entity', async () => {
+      const id = 5;
+      jest.spyOn(mockCarEntity, 'findOne');
+      const result = await carRepository.getCar(id);
+
+      expect(mockCarEntity.findOne).toHaveBeenCalledWith({ where: { id: 5 } });
+      expect(result).toEqual(mockCarDto);
+      expect(result.id).toEqual(id);
+    });
+  });
+
+  describe('createCar', () => {
+    it('Call the TypeOrm create and save method and returns a DbCarDto entity', async () => {
+      jest.spyOn(mockCarEntity, 'create');
+      jest.spyOn(mockCarEntity, 'save');
+      const result = await carRepository.createCar(mockNewCarDto);
+
+      expect(mockCarEntity.create).toHaveBeenCalledWith(mockNewCarDto);
+      expect(mockCarEntity.save).toHaveBeenCalledWith(mockNewCarDto);
+      expect(result).toEqual(mockCarDto);
+    });
+  });
+
+  describe('updateCar', () => {
+    it('Call the TypeOrm preload and save method and returns a DbCarDto entity', async () => {
+      jest.spyOn(mockCarEntity, 'preload');
+      jest.spyOn(mockCarEntity, 'save');
+      const result = await carRepository.updateCar(mockUpdateCarDto);
+
+      expect(mockCarEntity.preload).toHaveBeenCalledWith(mockUpdateCarDto);
+      expect(mockCarEntity.save).toHaveBeenCalledWith(mockUpdateCarDto);
+      expect(result).toEqual(mockCarDto);
+      expect(result.id).toEqual(mockUpdateCarDto.id);
+    });
+  });
+
+  describe('deleteCar', () => {
+    it('Call the TypeOrm delete method and returns a DeleteResult entity', async () => {
+      const id = 5;
+      jest.spyOn(mockCarEntity, 'delete');
+      const result = await carRepository.deleteCar(id);
+
+      expect(mockCarEntity.delete).toHaveBeenCalledWith(id);
+      expect(result).toEqual(mockDeleteResult);
     });
   });
 });
